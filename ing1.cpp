df073ed60714163517de80d5288d2085cb9a1b89
Your Name <you@example.com>
2023-04-25 19:07:33 +0500

    first commit

---
+++ ing1.cpp
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "math_3d.h"
+
+GLuint VBO;
+
+
+ 
+
+
+
+
+static void RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT);//очистка буфера кадра
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);// функция отрисовки треугольника
+
+    glDisableVertexAttribArray(0);
+
+    glutSwapBuffers();//меняет фоновый и буфер кадра местами
+}
+
+
+static void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+}
+
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[3];//массив вершин
+
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = Vector3f(1.0f, -1.0f, 0.0f);
+    Vertices[2] = Vector3f(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); //инициализируем glut
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);//настройка опций
+    //параметры окна
+    glutInitWindowSize(1024, 768);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 02");
+
+    InitializeGlutCallbacks();
+
+    // инициализация glew и проверка на ошибки
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //устанавливает цвет
+
+    CreateVertexBuffer();
+
+
+    glutMainLoop();
+
+    return 0;
+}
